bf405177fb5088a65511718ad2d5892c78a64d1b
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-10-18 10:10:28 +0530

    test: make tutorial checks robust (require index.jmd and Project.toml only); docs: minor copy edits

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,19 @@ function tests()
     readdir(rootdir),
   )
   for dir in dirs
-    subdirs = readdir(joinpath(rootdir, dir))
+    subdirs = filter(y -> isdir(joinpath(rootdir, dir, y)), readdir(joinpath(rootdir, dir)))
     for subdir in subdirs
-      prefix = joinpath(rootdir, dir, subdir, subdir)
+      prefix = joinpath(rootdir, dir, subdir, "index")
       println("Verifying files in $dir/$subdir")
-      for suffix in [".jmd", ".html", ".ipynb", ".jl"]
-        spc = " "^(6 - length(suffix))
-        print("  $subdir$suffix$spc exists…… ")
-        @assert isfile(prefix * suffix)
-        println("✓")
-      end
+      # Require the source markdown to be present
+      print("  index.jmd   exists…… ")
+      @assert isfile(prefix * ".jmd")
+      println("✓")
+
+      # Optional: ensure each tutorial folder has a Project.toml for deps
+      print("  Project.toml exists…… ")
+      @assert isfile(joinpath(rootdir, dir, subdir, "Project.toml"))
+      println("✓")
     end
   end
 end
